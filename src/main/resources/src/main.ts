48bb3867f1c583b2bf9587e442bc5ac5e8d50887
justandreyb <justandreyb@gmail.com>
2017-04-30 23:00:32 +0300

    Revert "Initial commit for Angular"
    This reverts commit 656ddf08917e66e06e91fb07374629d205e03fab.

--- src/main/resources/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule }  from './app/root/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
